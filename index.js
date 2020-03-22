b896d35d3d9b63b8cd10fdcde4d0cde43308c611
Alexej <alexejdubow@gmail.com>
2020-03-22 13:33:10 +0200

    done eventListener keydown

--- index.js
+++ index.js
@@ -3,69 +3,89 @@
 import galleryArray from "./gallery-items.js";
 
 const ul = document.querySelector(".js-gallery");
-
-const img = galleryArray.map((item) => {
-  let images = document.createElement("img");
-  images.setAttribute("src", item.preview);
-  images.setAttribute("data-source", item.original);
-  images.setAttribute("alt", item.description);
-  images.classList.add("gallery__image");
-  return images;
-});
-
-const a = img.map((item) => {
-  let a = document.createElement("a");
-  a.setAttribute("href", item.original);
-  a.classList.add("gallery__link");
-  a.append(item);
-  return a;
-});
-
-const li = a.map((item) => {
-  let list = document.createElement("li");
-  list.classList.add("gallery__item");
-  list.append(item);
-  return list;
-});
-
-ul.append(...li);
-
+ul.append(...createListTags());
 ul.addEventListener("click", openModal);
 
-function openModal(event) {
-  if (event.target == event.currentTarget) {
-    return;
-  }
-  event.preventDefault();
+const modalWrapper = document.querySelector(".js-lightbox");
+const modal = document.querySelector(".lightbox__content");
 
-  const divModal = document.querySelector(".js-lightbox");
-  divModal.classList.add("is-open");
-  const imgModal = document.querySelector(".lightbox__image");
-  imgModal.src = "";
-  imgModal.src = event.target.dataset.source;
-}
+modal.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) closeModal();
+});
+
+const imgModal = document.querySelector(".lightbox__image");
 
 const buttonClose = document.querySelector('[data-action="close-lightbox"]');
 buttonClose.addEventListener("click", closeModal);
-const overlay = document.querySelector(".lightbox__overlay");
-overlay.addEventListener("click", closeModal);
+
+function createListTags() {
+  let listTags = galleryArray.map((item) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", item.preview);
+    img.setAttribute("data-source", item.original);
+    img.setAttribute("alt", item.description);
+    img.classList.add("gallery__image");
+
+    const link = document.createElement("a");
+    link.setAttribute("href", item.original);
+    link.classList.add("gallery__link");
+
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    link.append(img);
+    li.append(link);
+
+    return li;
+  });
+  return listTags;
+}
+
+function openModal(event) {
+  if (event.target !== event.currentTarget) {
+    event.preventDefault();
+
+    modalWrapper.classList.add("is-open");
+    imgModal.src = "";
+    imgModal.src = event.target.dataset.source;
+  }
+}
+
+function closeModal() {
+  modalWrapper.classList.remove("is-open");
+}
+const arrOriginalLinks = galleryArray.map((el) => el.original);
+
+/*********Method #1*********/
 
 document.addEventListener("keydown", (e) => {
   if (e.code == "Escape") {
     closeModal();
-  }
-  if (e.code == "ArrowRight") {
-    // console.log("ArrowRight")
-    // const imgModal = document.querySelector(".lightbox__image");
-
-    // console.dir(imgModal);
-  }
-  if (e.code == "ArrowLeft") {
-    // console.log("ArrowLeft")
+  } else if (e.code == "ArrowRight") {
+    nextImgSrc();
+  } else if (e.code == "ArrowLeft") {
+    prevImgSrc();
   }
 });
 
-function closeModal() {
-  const divModal = document.querySelector(".js-lightbox");
-  divModal.classList.remove("is-open");
+function nextImgSrc() {
+  const arrOriginalLinks = galleryArray.map((el) => el.original);
+  const currentImageSource = arrOriginalLinks.findIndex(
+    (el) => el === imgModal.src
+  );
+  if (currentImageSource !== arrOriginalLinks.length - 1) {
+    const nextImg = arrOriginalLinks[currentImageSource + 1];
+    imgModal.src = nextImg;
+  }
+}
+
+function prevImgSrc() {
+  const arrOriginalLinks = galleryArray.map((el) => el.original);
+  const currentImageSource = arrOriginalLinks.findIndex(
+    (el) => el === imgModal.src
+  );
+  if (currentImageSource !== 0) {
+    const prevImg = arrOriginalLinks[currentImageSource - 1];
+    imgModal.src = prevImg;
+  }
 }
